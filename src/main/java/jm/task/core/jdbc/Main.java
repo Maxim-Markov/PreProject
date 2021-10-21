bedd919b942431b56fc4e454d1931104767b671d
Maxim-Markov <42976416+Maxim-Markov@users.noreply.github.com>
2021-10-21 18:26:14 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,12 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         //перед использование необходимо задать свои настройки подключения в database.properties
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Иван", "Ненахов", (byte) 23);
         userService.saveUser("Георгий", "Куршаков", (byte) 23);
